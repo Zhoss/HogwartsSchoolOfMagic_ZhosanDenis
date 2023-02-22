9500cacc2e16acee80436e32d782720e6ba3a1e5
Zhoss <joss.90@mail.ru>
2023-02-22 13:24:01 +1000

    Выполненное ДЗ к уроку 4.8 Управление схемами баз данных

---
+++ scripts423.sql
@@ -0,0 +1,7 @@
+SELECT students.name, students.age, faculty.name
+FROM students
+INNER JOIN faculty ON students.faculty_id = faculty.id;
+
+SELECT students.name, students.age, avatar.file_path
+FROM students
+INNER JOIN avatar ON students.id = avatar.student_id;
